05f90769013f53ca253b4d5a4861a9f07d91a012
Ellen Spertus <ellen.spertus@gmail.com>
2014-02-24 07:45:12 -0800

    Added ContentClient to repo.

---
+++ ContentClient/src/edu/mills/cs180a/contentclient/MainActivity.java
@@ -0,0 +1,67 @@
+
+package edu.mills.cs180a.contentclient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final int PICK_REQUEST = 1;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(i, PICK_REQUEST);
+    }
+
+    // Code adapted from section 11.17 of The Android Cookbook by Ian Darwin (O'Reilly).
+    // Returns the empty string if record has no email address.
+    private String getContactEmail(Uri contactUri) {
+        // Get id of contact.
+        Cursor contactCursor = getContentResolver().query(contactUri, null, null, null, null);
+        int columnIndexForId = contactCursor.getColumnIndex(ContactsContract.Contacts._ID);
+        String contactId = contactCursor.getString(columnIndexForId);
+
+        // Now that we have an id, we can request the email address.
+        Cursor emailsCursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, 
+                ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=" + contactId,
+                null, null);
+
+        // Get the (first) email address.
+        String email = "";
+        if (emailsCursor.moveToFirst()) {
+            email = emailsCursor.getString(emailsCursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Email.ADDRESS));
+            if (emailsCursor.moveToNext()) {
+                Log.d(TAG, "Additional emails ignored.");
+            }
+        } else {
+            Log.w(TAG, "No email address found for contact.");
+        }
+        
+        // Clean up before returning.
+        contactCursor.close();
+        emailsCursor.close();
+        return email;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_REQUEST && resultCode == RESULT_OK) {
+            // Get the id of the selected contact.
+            String email = getContactEmail(data.getData());
+            Toast.makeText(this, "Found email: " + email, Toast.LENGTH_LONG).show();
+        } else {
+            Log.w(TAG, "Did not pick contact.");
+        }
+    }
+}
